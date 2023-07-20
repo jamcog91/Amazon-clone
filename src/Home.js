95a817146522b066e72e450a4054cbc01562a79a
James Cogan <james.m.cogan@gmail.com>
2023-07-20 15:20:10 -0400

    product containers

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,19 @@ function Home() {
                     src="https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/a/AmazonStores/ATVPDKIKX0DER/7a5f6ce2fdbd715196440d05e2f0b1d0.w3000.h600._CR0%2C0%2C3000%2C600_SX1920_.jpg"
                     alt="amazon__background"
                 />
+                <div className="home__row"> 
+                    {/* {product} */}
+                    {/* {product} */}
+                </div>
+                <div className="home__row"> 
+                    {/* {product} */}
+                    {/* {product} */}
+                    {/* {product} */}
+                </div>
+                <div className="home__row"> 
+                    {/* {product} */}
+                    {/* {product} */}
+                </div>
             </div>
         </div>
     );
---
+++ src/Product.js
@@ -0,0 +1,10 @@
+import "./Product.css";
+
+function Product() {
+    return(
+        <div className="product">
+
+        </div>
+    );
+};
+export default Product;
