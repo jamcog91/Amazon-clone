7e20ededcccfd7c2ccb064ec74954c75e75ec21f
James Cogan <james.m.cogan@gmail.com>
2023-07-20 15:13:05 -0400

    background image

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,13 @@ import "./Home.css";
 function Home() {
     return(
         <div className="home">
-
+            <div className="home_container">
+                <img 
+                    className="home__img"
+                    src="https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/a/AmazonStores/ATVPDKIKX0DER/7a5f6ce2fdbd715196440d05e2f0b1d0.w3000.h600._CR0%2C0%2C3000%2C600_SX1920_.jpg"
+                    alt="amazon__background"
+                />
+            </div>
         </div>
     );
 };
