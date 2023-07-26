c4ff9197fe6c94d5b817df37d6b107e8f1fcc95d
James Cogan <james.m.cogan@gmail.com>
2023-07-26 18:51:50 -0400

    checkout product styling

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,6 +2,12 @@ import "./CheckoutProduct.css";
 import StarRateIcon from '@mui/icons-material/StarRate';
 
 function CheckoutProduct({ id, title, image, price, rating }) {
+
+    // const removeFromBasket = () => {
+
+    // };
+
+
     return(
         <div className="checkoutProduct">
             <img className="checkoutProduct__image" src={image} />
