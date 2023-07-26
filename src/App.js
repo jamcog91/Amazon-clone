273041e22e532c552987de2448b5a9b0f1c61d66
James Cogan <james.m.cogan@gmail.com>
2023-07-26 13:48:58 -0400

    404 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Header.js';
 import Home from './Home';
 import Checkout from './Checkout';
+import NoPage from './NoPage';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 
 
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/Checkout" element={<Checkout />}></Route>
+            <Route path="/*" element={<NoPage />}></Route>
           </Routes>
         </Router>
     </div>
---
+++ src/NoPage.js
@@ -0,0 +1,10 @@
+import "./NoPage.css";
+
+function NoPage() {
+    return(
+        <div>
+            <strong className="error__message">Sorry, we cannot find the page you are looking for.</strong>
+        </div>
+    )
+}
+export default NoPage;
