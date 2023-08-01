67915fd881bef07b25f4eabb478248679422da41
James Cogan <james.m.cogan@gmail.com>
2023-08-01 13:06:16 -0400

    firebase login

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB8UTqgnhwwOJqHYhGPhUZgATuYkOB6xOc",
+  authDomain: "clone-c9c5a.firebaseapp.com",
+  projectId: "clone-c9c5a",
+  storageBucket: "clone-c9c5a.appspot.com",
+  messagingSenderId: "1040147683396",
+  appId: "1:1040147683396:web:e46edf5decbd1477804411",
+  measurementId: "G-X6E7YGGPHB"
+};
